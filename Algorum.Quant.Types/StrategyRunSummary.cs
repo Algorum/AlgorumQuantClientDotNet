fa305dded3f5e486276d6ea35f6464beb66ae18a
Adityanand Pasumarthi <aditya.pasumarthi@algorum.in>
2021-11-19 21:10:37 +0530

    Added strategy run summary related types.

---
+++ Algorum.Quant.Types/StrategyRunSummary.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorum.Quant.Types
+{
+   public class StrategyRunSummary
+   {
+      public string StrategyId
+      {
+         get;
+         set;
+      }
+
+      public string RunId
+      {
+         get;
+         set;
+      }
+
+      public StrategyLaunchMode LaunchMode
+      {
+         get;
+         set;
+      }
+
+      public BrokeragePlatform BrokeragePlatform
+      {
+         get;
+         set;
+      }
+
+      public DateTime StartTime
+      {
+         get;
+         set;
+      }
+
+      public DateTime EndTime
+      {
+         get;
+         set;
+      }
+
+      public string BacktestStartDate
+      {
+         get;
+         set;
+      }
+
+      public string BacktestEndDate
+      {
+         get;
+         set;
+      }
+
+      public double PL
+      {
+         get;
+         set;
+      }
+
+      public double Capital
+      {
+         get;
+         set;
+      }
+
+      public List<Order> Orders
+      {
+         get;
+         set;
+      }
+
+      public Dictionary<string, Dictionary<string, object>> SymbolStatsMap
+      {
+         get;
+         set;
+      }
+   }
+}
